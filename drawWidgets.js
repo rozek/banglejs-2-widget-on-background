9cb8f66adaede52ad634437b8faf2f1f2db9a8e1
Andreas Rozek <A.Rozek@gmx.de>
2022-01-09 12:19:55 +0100

    Create drawWidgets.js

---
+++ drawWidgets.js
@@ -0,0 +1,34 @@
+(function () {
+  Bangle.drawWidgets = function () {
+    var w = g.getWidth(), h = g.getHeight();
+
+    var pos = {
+      tl:{x:0,   y:0,    r:0, c:0}, // if r==1, we're right->left
+      tr:{x:w-1, y:0,    r:1, c:0},
+      bl:{x:0,   y:h-24, r:0, c:0},
+      br:{x:w-1, y:h-24, r:1, c:0}
+    };
+
+    if (global.WIDGETS) {
+      for (var wd of WIDGETS) {
+        var p = pos[wd.area];
+        if (!p) continue;
+
+        wd.x = p.x - p.r*wd.width;
+        wd.y = p.y;
+
+        p.x += wd.width*(1-2*p.r);
+        p.c++;
+      }
+
+      g.reset();                                 // also loads the current theme
+
+      try {
+        for (wd of WIDGETS) {
+          g.setClipRect(wd.x,wd.y, wd.x+wd.width-1,23);
+          wd.draw(wd);
+        }
+      } catch (e) { print(e); }
+    }
+  }
+})();
